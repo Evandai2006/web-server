ec275ed615397353e7dda5f8ea78742e3ba79e0d
Evandai2006 <evandai2006@outlook.com>
2026-04-20 21:33:26 +0800

    2026/4/20update2 New thread mgmt methods added bug fixes

--- simpleHTMLServer.py
+++ simpleHTMLServer.py
@@ -6,12 +6,17 @@ from datetime import datetime, timedelta
 import os
 import logging
 
-tz = 8# UTC+8 HKT ONLY, if you want to use this from another time zone, pls change.
-max_threads = 500
-active_clients = []
-lock = threading.Lock()
-timeout_duration = 3600#a maximum visit duration to the website is 1 hour.
+'''USER PLEASE SET THESE PARAMETERS'''
+tz = 8# UTC+8 is for HKT ONLY, if you want to use this from another time zone, pls change.
+max_threads = 50#Max. # of threads running in the server
+timeout_duration = 1#a maximum visit duration to the website, set according to need.
 cleanup_time = 4#local time of auto clean up time(o'clock only, set within the timezone(tz))
+TC_strategy = 'A'#A for only cleaning the threads once a day, B for queue(limited time session per client, AB for both.)
+visit_blacklist = {'restricted.html'}
+cli_whitelist = {}
+'''================================'''
+
+
 
 r200 = b'HTTP/1.1 200 OK\n\n'
 r304 = b'HTTP/1.1 304 Not Modified\n\n'
@@ -20,14 +25,6 @@ r403 = b'HTTP/1.1 403 Forbidden\n\nAccess Denied'
 r404 = b'HTTP/1.1 404 Not Found\n\nFile Not Found'
 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-visit_blacklist = {'restricted.html'}
-cli_whitelist = {}
-
-global file_mod_times
-file_mod_times = {}
-
 logging.basicConfig(
     level=logging.INFO,
     format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -35,12 +32,21 @@ logging.basicConfig(
     filename = 'server.log',
 )
 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+global file_mod_times
+file_mod_times = {}
 def get_file_mod_times():
     for filename in ['favicon.ico', 'root.html', 'page1.html', 'restricted.html','polyu.png']:
         file_path = os.path.join(BASE_DIR, filename)
         if os.path.isfile(file_path):
             file_mod_times[f'/{filename}'] = os.path.getmtime(file_path)
     print("File modified time refreshed")
+
+
+active_clients = []
+lock = threading.Lock()
+timeout_duration *= 3600
 
 class ClientThread:
     def __init__(self, client_connection, client_address):
@@ -61,21 +67,21 @@ def handle_client(client_thread):
 
     logging.info(f"Connection from{client_thread.client_address} established.")
 
-
-
     while not client_thread.should_exit.is_set():
-        elapsed_time = time.time() - client_thread.start_time
-        if elapsed_time > timeout_duration:
-            print(f"Terminating client thread due to timeout: exceeding {elapsed_time / 3600:.2f} hours.")
-            client_thread.should_exit.set()
-            break
+        if 'B' in TC_strategy:
+            elapsed_time = time.time() - client_thread.start_time
+            if elapsed_time > timeout_duration:
+                print(f"Terminating client thread due to timeout: exceeding {elapsed_time / 3600:.2f} hours.")
+                client_thread.should_exit.set()
+                break#this management method isn't preferred for not crowded servers
 
         request = client_thread.client_connection.recv(1024).decode()
-        get_file_mod_times()
+
         if not request:
             client_thread.client_connection.sendall(r400)
             break
 
+        get_file_mod_times()
         print('Request:\n')
         print(request)
         logging.info(f"Client requesting: {request}")
@@ -131,7 +137,7 @@ def handle_client(client_thread):
                                         response_header = f'HTTP/1.1 200 OK\nContent-Type: image/jpg\nContent-Length: {len(content)}\nLast-Modified: {time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(last_modified))}\n\n'
                                     else:
                                         response_header = f'HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: {len(content)}\nLast-Modified: {time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(last_modified))}\n\n'
-                                    #separate the response of head and get
+
                                     if request_type == 'HEAD':
                                         response = response_header.encode()
                                     else:
@@ -148,13 +154,11 @@ def handle_client(client_thread):
                     logging.info(f"Server response: {r400}, unsupported connection type.")
                     break
 
-
-
             print(response)
             client_thread.client_connection.sendall(response)
             logging.info(f"Server response: {response}")
 
-            if not 'keep-alive' in headers[2]:
+            if 'close' in headers[2]:
                 break
 
         except Exception as e:
@@ -169,20 +173,22 @@ def handle_client(client_thread):
 
 def reset_connections():#terminate all the threads at set time to free server resources.
     while True:
-        now = datetime.utcnow()
-        nextrun = now.replace(hour=cleanup_time, minute=0, second=0, microsecond=0) + timedelta(days=1) - tz*3600
-        sleeptime = (nextrun - now).total_seconds()
-        time.sleep(sleeptime)
-        cleanup_timeout = 120
-        print(f"Resetting connections... Est. waiting time: {cleanup_timeout} sec.")
-        with lock:
-            for thread in active_clients:
-                if thread.is_alive():
-                    thread.should_exit.set()#set exit flag to the thread
-                    thread.join(timeout = cleanup_timeout)
-                active_clients.remove(thread)
-            print("Process completed.")
-            logging.info("All threads cleared at set time.")
+        if 'A' in TC_strategy:
+            now = datetime.utcnow()
+            nextrun = now.replace(hour=cleanup_time, minute=0, second=0, microsecond=0) + timedelta(hours=24-tz)
+            sleeptime = (nextrun - now).total_seconds()
+            time.sleep(sleeptime)
+            cleanup_timeout = 120
+            print(f"Resetting connections... Est. waiting time: {cleanup_timeout} sec.")
+            with lock:
+                for thread in active_clients:
+                    if thread.is_alive():
+                        if thread.start_time - time.time() > timeout_duration:#to avoid that user happen to just established the thread
+                            thread.should_exit.set()#set exit flag to the thread
+                            thread.join(timeout = cleanup_timeout)
+                            active_clients.remove(thread)
+                print(f"Process completed. {len(active_clients)} threads remaining, due to: thread running time below minimum. remaining thread(s): {active_clients}")
+                logging.info(f"Threads cleared at set time. {len(active_clients)} threads remaining, due to: thread running time below minimum. remaining thread(s): {active_clients}")
 
 def thread_mgmt():
     SERVER_HOST = 'localhost'
@@ -199,16 +205,16 @@ def thread_mgmt():
     while True:
         client_connection, client_address = server_socket.accept()
         print(f'Connection from {client_address}')
-
         with lock:
-            if len(active_clients) < max_threads:
+            if max_threads > len(active_clients):
                 client_thread = ClientThread(client_connection, client_address)
                 thread = threading.Thread(target=handle_client, args=(client_thread,))
                 active_clients.append(thread)
                 thread.start()
             else:
-                print('Max client connections reached. Connection refused.')
-                logging.info(f'Refused connection from {client_address}, reason: max client connections reached.')
+                logging.info(f'Refused connection from {client_address}, ERR MSG: Too much threads established.')
+                print("CAUTION:Max client connections reached. Connection refused.\nIf the server is facing hi traffic, change the thread control strategy?")
+        print(f"Threading control: {TC_strategy}\ncurrent running threads: {active_clients}")
 
 
 if __name__ == "__main__":
